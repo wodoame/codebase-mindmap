182ef3cf9cfa8c45471ff152578d182fee3f9fe5
Wodoame <bmwodoame@gmail.com>
2025-09-10 10:08:26 +0000

    feat: create focusedElement markup

--- core/templates/core/components/modals/focusedElement.py
+++ core/templates/core/components/modals/focusedElement.py
@@ -1,8 +1,8 @@
 from django_components import Component, register
 
-@register('editorModal')
-class EditorModal(Component):
-    template_name = 'editorModal.html'
+@register('focusedElement')
+class FocusedElement(Component):
+    template_name = 'focusedElement.html'
 
     def get_template_data(self, args, kwargs, slots, context):
         return kwargs
