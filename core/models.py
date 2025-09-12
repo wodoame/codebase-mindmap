16a9dbfcf4781607a81fa24f22e6261ade068a32
Wodoame <bmwodoame@gmail.com>
2025-09-12 20:12:57 +0000

    alter fields of model MindMap

--- core/models.py
+++ core/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class MindMap(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True, null=True)
-    data = models.JSONField(default=dict)  # Store mind map data as JSON
+    data = models.JSONField(default=dict, blank=True, null=True)  # Store mind map data as JSON
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
