a8afc42a6b60dcbe32db0421b907ea40e1bfd064
Wodoame <bmwodoame@gmail.com>
2025-09-12 10:00:46 +0000

    feat: create MindMap model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class MindMap(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+    data = models.JSONField(default=dict)  # Store mind map data as JSON
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
