03cbf4d006feab352604bddb1ec4b5eafabe4747
Wodoame <bmwodoame@gmail.com>
2025-09-20 19:46:23 +0000

    create a default node for a newly created mindmap

--- core/models.py
+++ core/models.py
@@ -1,9 +1,14 @@
 from django.db import models
 
+
+def default_mindmap_data():
+    return {"name": "root", "HTML": ""}
+
+
 class MindMap(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True, null=True)
-    data = models.JSONField(default=dict, blank=True, null=True)  # Store mind map data as JSON
+    data = models.JSONField(default=default_mindmap_data, blank=True, null=True)  # Store mind map data as JSON
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
