26897bc1b227e09ec4ffe2ffb06d23583b39235d
Wodoame <bmwodoame@gmail.com>
2025-09-21 22:13:50 +0000

    add variable classes to adjust modal width

--- core/templates/core/components/modals/baseModal.py
+++ core/templates/core/components/modals/baseModal.py
@@ -1,8 +1,13 @@
-from django_components import Component, register
+from django_components import Component, register, merge_attributes
 
 @register('baseModal')
 class BaseModal(Component):
     template_name = 'baseModal.html'
     
-    def get_context_data(self, *args, **kwargs):
+    def get_template_data(self, args, kwargs, slots, context):
+        is_editor_modal = kwargs.get("id") == 'editor-modal'
+        res = merge_attributes(
+            {"class": {"w-[80%]": is_editor_modal, "md:max-w-md w-full": not is_editor_modal}}
+        )
+        kwargs['classes'] = res
         return kwargs
