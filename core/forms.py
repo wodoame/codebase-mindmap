c618af6bb2bcce82a5f064e5573a8d0ca0c6935c
Wodoame <bmwodoame@gmail.com>
2025-09-24 13:39:51 +0000

    feat: add ability to fork a map

--- core/forms.py
+++ core/forms.py
@@ -4,7 +4,7 @@ from .models import MindMap
 class MindMapForm(forms.ModelForm):
     class Meta:
         model = MindMap
-        fields = ["title", "description"]
+        fields = ["title", "description", "data"]
         widgets = {
             "title": forms.TextInput(attrs={
                 "class": "block w-full rounded-md border px-3 py-2",
@@ -15,5 +15,11 @@ class MindMapForm(forms.ModelForm):
                 "class": "block w-full rounded-md border px-3 py-2",
                 "placeholder": "Optional description",
                 "rows": 4,
+            }),
+            "data": forms.Textarea(attrs={
+                "class": "block w-full rounded-md border px-3 py-2",
+                "placeholder": "Optional data",
+                "rows": 4,
+                "spellcheck": "false",
             }),
         }
